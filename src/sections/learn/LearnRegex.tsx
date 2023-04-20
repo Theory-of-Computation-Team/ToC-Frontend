4b8fe963b74ed9bdee1ad416e160795dd009be7c
NuntNonthicha <69186692+NuntNonthicha@users.noreply.github.com>
2023-04-21 02:31:27 +0700

    edit header in learn page

--- src/sections/learn/LearnRegex.tsx
+++ src/sections/learn/LearnRegex.tsx
@@ -1,16 +1,13 @@
-import SearchBar from "@/commons/components/SearchBar";
 import React from "react";
 
 const LearnRegex = () => {
   return (
     <div id="learn" className="w-full py-6 md:py-16 md:px-32">
-      <div className="container mx-auto flex flex-col items-center px-8 py-8 md:py-12 gap-4">
-        <h1 className="inline-block group font-bold text-xl md:text-2xl xl:text-4xl tracking-wide transition-all duration-300 ease-in-out">
+      <div className="container mx-auto flex flex-col items-start px-8 py-8 md:py-12 gap-4">
+        <h1 className="relative my-8 md:my-12 inline-block group font-bold text-xl md:text-3xl xl:text-4xl tracking-wide transition-all duration-300 ease-in-out">
           หลักการ Regular Expression
-          <span className="block my-2 h-1 w-[30%] group-hover:w-full transition-all duration-500 ease-out bg-primary"></span>
+          <span className="absolute left-0 -bottom-5 my-2 h-1 w-[30%] group-hover:w-full transition-all duration-500 ease-out bg-primary"></span>
         </h1>
-
-        {/* <SearchBar /> */}
       </div>
     </div>
   );
