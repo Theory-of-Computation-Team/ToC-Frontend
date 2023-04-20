2d4832cf4d2225eb6ebe8651072f41e51712b4f0
Chanon <69115498+chanongul@users.noreply.github.com>
2023-04-21 00:21:38 +0700

    Temples search header init

--- src/sections/temples/TemplesHeader.tsx
+++ src/sections/temples/TemplesHeader.tsx
@@ -1,7 +1,17 @@
-import React from 'react'
+import React from "react";
+import { BiSearch } from "react-icons/bi";
 
 export default function TemplesHeader() {
   return (
-    <div>TemplesHeader</div>
-  )
+    <div className="container w-[80%] flex justify-between items-center">
+      <h1 className="relative my-8 md:my-12 inline-block group font-bold text-xl md:text-3xl xl:text-4xl tracking-wide transition-all duration-300 ease-in-out">
+        รายชื่อวัด
+        <span className="absolute left-0 -bottom-5 my-2 h-1 w-[30%] group-hover:w-full transition-all duration-500 ease-out bg-primary"></span>
+      </h1>
+      <div className="flex items-center border-[.5px] border-black rounded-full py-2 px-4 w-80">
+        <BiSearch />
+        <input placeholder="ค้นหารายชื่อวัด..." className="" />
+      </div>
+    </div>
+  );
 }
