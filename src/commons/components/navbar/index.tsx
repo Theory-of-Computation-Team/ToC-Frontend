053cf33d9aca236da4a2b07ac5072f0145df9112
NuntNonthicha <69186692+NuntNonthicha@users.noreply.github.com>
2023-04-20 16:42:05 +0700

    install react-icons

--- src/commons/components/navbar/index.tsx
+++ src/commons/components/navbar/index.tsx
@@ -1,8 +1,42 @@
 import * as React from "react";
 import Image from "next/image";
+import Link from "next/link";
 
 const Navbar = () => {
-  return <div>navbar test</div>;
+  return (
+    <div className="fixed w-full bg-blue-300">
+      <div className="flex space-x-4">
+        <Link
+          href="/about#about-objective"
+          className="text-[#999999] capitalize hover:text-primary"
+          replace
+        >
+          หน้าหลัก
+        </Link>
+        <Link
+          href="/about#about-story"
+          className="text-[#999999] capitalize hover:text-primary"
+          replace
+        >
+          รายชื่อวัด
+        </Link>
+        <Link
+          href="/about#area-focus"
+          className="text-[#999999] capitalize hover:text-primary"
+          replace
+        >
+          หลักการ Regular Expression
+        </Link>
+        <Link
+          href="/about#area-focus"
+          className="text-[#999999] capitalize hover:text-primary"
+          replace
+        >
+          หลักการ Regular Expression
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
